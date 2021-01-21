a5b33ac65ff35d55c0cdcf59aa179aad96d6142c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-01-22 01:44:12 +0900

    11:00まで

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -2,10 +2,21 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Page;
 use Livewire\Component;
 
 class Pages extends Component
 {
+    public $modalFormVisible = false;
+    public $slug;
+    public $title;
+    public $content;
+
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
     public function render()
     {
         return view('livewire.pages');
