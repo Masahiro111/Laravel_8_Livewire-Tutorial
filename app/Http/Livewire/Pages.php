5e3a69d689ba1696b5e193f7327e9d42a010b8f2
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-01-23 04:12:23 +0900

    1-18:48

--- app/Http/Livewire/Pages.php
+++ app/Http/Livewire/Pages.php
@@ -12,9 +12,24 @@ class Pages extends Component
     public $title;
     public $content;
 
+    public function create()
+    {
+        Page::create($this->modelData());
+        $this->modalFormVisible = false;
+    }
+
     public function createShowModal()
     {
         $this->modalFormVisible = true;
+    }
+
+    public function modelData()
+    {
+        return [
+            'title' => $this->title,
+            'slug' => $this->slug,
+            'content' => $this->content,
+        ];
     }
 
     public function render()
