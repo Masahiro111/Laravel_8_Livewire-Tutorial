8cda9194f1cb1c466cf77b3ade045c136b46689c
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-01-27 01:15:59 +0900

    add tailwind

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -1,3 +1,11 @@
 <div>
-    <h1>{{$title}}</h1>
+    <div class="sm:bg-blue-500 md:bg-red-500 border p-5 text-gray-100 bg-gray-400 text-3xl">{{ $title }}</div>
+    <div class="lg:flex">
+        <div class="border p-5 text-left lg:w-1/2">
+            {!! $content !!}
+        </div>
+        <div class="border bg-gray-400 lg:w-1/2">
+            <img class="w-full h-full object-cover object-center" src="{{ 'img/mountain.jpg' }}" alt="">
+        </div>
+    </div>
 </div>
