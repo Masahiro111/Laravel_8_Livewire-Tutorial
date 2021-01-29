d832f347ff1f703f9a3f94775083cbd1a795a94a
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-01-29 19:01:51 +0900

    9:16:50

--- app/Http/Livewire/NavigationMenus.php
+++ app/Http/Livewire/NavigationMenus.php
@@ -3,11 +3,38 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\NavigationMenu;
+use Livewire\WithPagination;
 
 class NavigationMenus extends Component
 {
+
+    use WithPagination;
+
+    public $modalFormVisible = false;
+
+    public $modelId;
+    public $label;
+    public $slug;
+    public $sequence = 1;
+    public $type = 'SidebarNav';
+
+
+    public function read()
+    {
+        return NavigationMenu::paginate(5);
+    }
+
+
+    public function createShowModal()
+    {
+        $this->modalFormVisible = true;
+    }
+
     public function render()
     {
-        return view('livewire.navigation-menus');
+        return view('livewire.navigation-menus', [
+            'data' => $this->read(),
+        ]);
     }
 }
